9cc899fa638b69887978dbea9ae0a95857bc3fd0
Yahia_g <yahiaahmed2yahia@hotmail.com>
2025-04-18 23:33:55 +0200

    user parameters missing login

---
+++ src/organizer.java
@@ -0,0 +1,13 @@
+public class organizer {
+
+    private String organizerName;
+    private String contactInfo;
+    private int numOfEvents;
+    private Event[] currentEvents = new Event[numOfEvents];
+    private double balance;
+
+    
+
+
+
+}
