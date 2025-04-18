482ce71b59584043d3ad4cef7e30b9e0b5786bfa
eyad-dev <24P0240@eng.asu.edu.eg>
2025-04-18 08:50:53 +0200

    Eyad's part

---
+++ src/Admin.java
@@ -0,0 +1,38 @@
+public class Admin {
+
+
+    //Attributes
+    private String role;
+    private String workingHours;
+
+
+    //Constructors//
+    Admin(){}
+
+    Admin(String role, String workingHours){
+        this.role = role;
+        this.workingHours = workingHours;
+    }
+
+
+    //Methods
+    public String getRole() {
+        return role;
+    }
+
+    public String getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setWorkingHours(String workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    //till User class is made
+    //Methods to be added:
+    //addRooms, viewEvents, viewAttendees, viewOrganizers, login, register, toString, equals
+}
