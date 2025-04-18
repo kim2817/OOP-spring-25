91a15dc9b9855dfd802e739332a0d28d6137b647
Yahia_g <yahiaahmed2yahia@hotmail.com>
2025-04-18 19:02:46 +0200

    FInally

---
+++ src/User.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class User {
+    String email;
+    String username;
+    String contactNo;
+    String password;
+    Date dateOfBirth;
+    String address;
+    double balance;
+    gender gen;
+
+    User(){
+
+    }
+
+
+
+
+
+
+}
