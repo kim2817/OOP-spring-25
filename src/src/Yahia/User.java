28af9ca2cbab21b9f539f3839d14aa3dd9096660
Yahia_g <yahiaahmed2yahia@hotmail.com>
2025-04-19 20:39:55 +0200

    f

--- src/src/Yahia/User.java
+++ src/src/Yahia/User.java
@@ -4,24 +4,70 @@ import java.util.Date;
 import x3mara.*;
 
 abstract public class User implements HasID {
-    String email;
-    String username;
-    String contactNo;
-    String password;
-    Date dateOfBirth;
-    String address;
-    double balance;
-    gender gen;
-    String ID;
+    protected String email;
+    protected String username;
+    protected String contactNo;
+    protected String password;
+    protected Date dateOfBirth;
+    protected String address;
+    protected double balance;
+    protected gender gen;
+    protected String ID;
 
-    abstract public String getEmail();
-    abstract public String getUsername();
-    abstract public String getContactNo();
-    abstract public String getPassword();
-    abstract public gender getGen();
-    abstract public double getBalance();
-    abstract public Date getDateOfBirth();
-    abstract public String getAddress();
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getContactNo() {
+        return contactNo;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public gender getGen() {
+        return gen;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setGen(gender gen) {
+        this.gen = gen;
+    }
+
+    @Override
+    public String getID() {
+        return ID;
+    }
 
     @Override
     abstract public String toString();
