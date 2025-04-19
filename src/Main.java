7ffd39169275431da2f1a94e161c7b96ec5e55df
Ahmed Emara <ahmed6emara@gmail.com>
2025-04-19 11:44:12 +0200

    v0.00001 (doesnt even compile)

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Wallet[] w = new Wallet[10];
+        for(int i=0;i<10;i++){
+            w[i] = new Wallet();
+        }
+        System.out.println("work");
+    }
+}
