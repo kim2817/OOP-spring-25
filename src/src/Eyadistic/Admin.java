133f214fbf68a08093ce0715c6722f0f67f4f294
eyad-dev <24P0240@eng.asu.edu.eg>
2025-04-19 20:34:28 +0200

    commit

--- src/src/Eyadistic/Admin.java
+++ src/src/Eyadistic/Admin.java
@@ -1,7 +1,9 @@
 package Eyadistic;
 import Yahia.User;
-import x3mara.Schedule;
-import x3mara.Database;
+import Yahia.gender;
+import x3mara.*;
+
+import java.util.Date;
 
 
 public class Admin extends User{
@@ -46,14 +48,70 @@ public class Admin extends User{
 
     public void addRooms(String roomName, String roomCapacity, int rentPrice, double bookedSlots, Schedule[] bookedList){
     }
+
     @Override
-    public String toString(){
-        return "Wok";
+    public String getEmail() {
+        return "";
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public String getContactNo() {
+        return "";
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public gender getGen() {
+        return null;
+    }
+
+    @Override
+    public double getBalance() {
+        return 0;
+    }
+
+    @Override
+    public Date getDateOfBirth() {
+        return null;
+    }
+
+    @Override
+    public String getAddress() {
+        return "";
     }
     @Override
     public String getID() {
         return ID;
     }
+
+    @Override
+    public String toString(){
+       return "User{" +
+                "email='" + getEmail() + '\'' +
+                ", username='" + getUsername() + '\'' +
+                ", contactNo='" + getContactNo() + '\'' +
+                ", password='" + getPassword() + '\'' +
+                ", dateOfBirth=" + getDateOfBirth() +
+                ", address='" + getAddress() + '\'' +
+                ", balance=" + getBalance() +
+                ", gen=" + getGen() +
+                ", ID='" + getID() + '\'' +
+                '}';
+    }
+
     //till User class is made
     //Methods to be added:
     //addRooms, viewEvents, viewAttendees, viewOrganizers, login, register, toString, equals//
