4ecd3154f0c35d79ec4be87537fceac2d9d893c4
Omar <74670247+omar887@users.noreply.github.com>
2025-04-19 15:08:09 +0200

    Add files via upload

---
+++ src/src/Omar/Main.java
@@ -0,0 +1,76 @@
+import java.util.UUID;
+
+class Attendee {
+    private String id;
+    private int age;
+    private String city;
+    private Wallet balance;
+    private int[][] Purchasedtickets;
+
+    Attendee() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    Attendee(int age, String city, int[][] Purchasedtickets, double balance) {
+        this.id = UUID.randomUUID().toString();
+        this.age = age;
+        this.city = city;
+        this.balance = new Wallet(balance);
+        this.Purchasedtickets = Purchasedtickets;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Wallet getBalance() {
+        return balance.getBalance();
+    }
+    public void Attendeedeposit(double money){
+        balance.deposit(money);
+    }
+
+    public int[][] getPurchasedtickets() {
+        return Purchasedtickets;
+    }
+
+    public Category chooseInterest() {
+        return new Category();
+    }
+
+    public Event showEvents() {
+        return new Event();
+    }
+
+    public void chooseEvent() {
+        ///
+    }
+
+    public void buyTickets(int noOfTickets, int eventID, int price) {
+        double total = price * noOfTickets;
+        if (balance.isSufficient(total)) {
+            balance.withdraw(total);
+            System.out.println("You have purchased" + noOfTickets + "for eventID" + eventID);
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    public void register() {
+
+    }
+
+    public String toString() {
+        return  "Attendee[age=" + age + ", city=" + city + ", balance=" + balance.getBalance() + "]";
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Attendee) {
+            return this.age == ((Attendee) o).age && this.city.equals(((Attendee) o).city);
+        } else return false;
+    }
+}
